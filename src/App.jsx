8ae276f93fd93183719531cc7c8ab5e23c1d021c
Mateo Bellini <bellinimateo872@gmail.com>
2023-10-12 15:45:11 -0300

    Header hecho

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import "./App.css";
+import Hero from "./components/Hero";
 import Header from "./components/common/Header";
 
 function App() {
   return (
     <>
       <Header />
-      <main></main>
+      <main>
+        <Hero />
+      </main>
     </>
   );
 }
---
+++ src/components/Hero.jsx
@@ -0,0 +1,9 @@
+const Hero = () => {
+  return (
+    <section className="container_hero">
+      <article></article>
+    </section>
+  );
+};
+
+export default Hero;
