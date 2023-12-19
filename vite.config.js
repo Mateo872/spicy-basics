e0f01f33372dafff7d44f36c54b58c2c5d2b3749
Mateo Bellini <bellinimateo872@gmail.com>
2023-12-19 14:35:01 +0100

    Configuración de vite

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    port: 3000,
+    hmr: {
+      port: 3000,
+    },
+  },
+  build: {
+    outDir: "dist/client",
+    ssrManifest: true,
+    rollupOptions: {
+      input: {
+        "entry-client": path.resolve(__dirname, "src/main.jsx"),
+        "entry-server": path.resolve(__dirname, "src/entry-server.jsx"),
+      },
+    },
+  },
+});
